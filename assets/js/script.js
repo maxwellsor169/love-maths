a9dc08494852b86e2cc4ca1b9f99dc902b3c34d5
Maxwell Sor <atm2chx3@students.codeinstitute.net>
2024-10-11 11:03:28 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);  
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    }else {
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw`Unknown game type: ${gameType}. Aborting!`;
     }
@@ -73,13 +75,14 @@ function checkAnswer() {
 function calculateCorrectAnswer() {
    let operand1 = parseInt(document.getElementById('operand1').innerText);
    let operand2 = parseInt(document.getElementById('operand2').innerText);
-   let operator = document.getElementById("operator").innerText;
+   let operator = document.getElementById('operator').innerText;
 
    if (operator === '+'){
        return [operand1 + operand2, "addition"];
    } else if (operator === 'x') {
        return [operand1 * operand2, "multiply"];
-
+   } else if(operator === '-') {
+       return [operand1 - operand2, "subtract"];
    } else {
         alert(`Unimplemented operator ${operator}`);
         throw`Unimplemented operator ${operator}. Aborting!`;
@@ -101,20 +104,32 @@ function incrementScore() {
  */
 
 function incrementWrongAnswer() {
+
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
 
 }
+
 function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "+";
-}
-function displaySubtractQuestion() {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
-function displayMultiplyQuestion() {
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "x";
+
+function displaySubtractQuestion() {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;;
+    document.getElementById('operator').textContent = "-";
+
+}
+
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
+
 }
